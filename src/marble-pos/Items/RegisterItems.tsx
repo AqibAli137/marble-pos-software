2022e58ddae8dd150c0b1961999dece9d4b90d9c
DESKTOP-TVR0LRO\dell <aqib.ali018634@gmail.com>
2023-02-28 02:08:36 +0500

    Create Item

--- src/marble-pos/Items/RegisterItems.tsx
+++ src/marble-pos/Items/RegisterItems.tsx
@@ -2,16 +2,45 @@ import React, { useEffect, useState } from "react";
 // import {useNavigate} from 'react-router-dom'
 import { Button } from "@mui/material";
 import Logo from "../../assets/sm-assets/sm-logo2.jpg";
+import { Item } from "../../Models/Item";
+import axios from "axios";
 
 export function RegisterItem() {
   // const navigate = useNavigate()
   const [itemName, setItemName] = useState("");
+  const [itemType, setItemType] = useState("فٹ");
   const [itemQuantity, setItemQuantity] = useState(1);
   const [itemCost, setItemCost] = useState(50);
   const [itemTotalprice, setItemTotalprice] = useState(50);
+
   useEffect(() => {
     setItemTotalprice(itemQuantity * itemCost);
   }, [itemQuantity, itemCost]);
+
+  const ItemRecord: Item = {
+    Id: 0,
+    ItemName: itemName,
+    CostOfItem: itemCost,
+    RealItemCost: itemCost,
+    TotalQuantity: itemQuantity,
+    TotalAmount: itemTotalprice,
+    TypeOfItem: itemType,
+  };
+  const createItem = () => {
+    {
+      itemName === "" || itemCost === 0 || itemType === "" || itemQuantity === 0
+        ? alert("براہ کرم مکمل تفصیلات درج کریں۔")
+        : axios
+            .post("https://localhost:7005/api/Item", ItemRecord)
+            .then((res) => {
+              alert("آپ کا نیا آئٹم ریکارڈ کامیابی سے Save ہو گیا۔");
+              setItemName("");
+            })
+            .catch((err) => {
+              alert("کچھ غلطی ہے، دوبارہ کوشش کریں۔");
+            });
+    }
+  };
 
   return (
     <>
@@ -20,8 +49,10 @@ export function RegisterItem() {
           <div className="col-12 col-lg-3 col-sm-3 col-md-3 text-center d-flex align-items-center">
             <div className="d-flex align-items-center justify-content-center">
               <div>
-                <img alt="Logo" src={Logo} 
-                style={{width:'120px', height:'120px', borderRadius:"100%"}}
+                <img
+                  alt="Logo"
+                  src={Logo}
+                  style={{ width: "120px", height: "120px", borderRadius: "100%" }}
                 />
                 <div className="d-flex justify-content-center">
                   <h3 style={{ lineHeight: "50px" }} className="col-12 text-center my-3">
@@ -44,10 +75,8 @@ export function RegisterItem() {
               <div className=" text-end mb-5">
                 <h1 className="text-dark fs-4 py-3">آئٹم کو رجسٹر کریں۔</h1>
               </div>
-              <form
+              <div
                 className="form w-100 col col-9 fv-plugins-bootstrap5 fv-plugins-framework"
-                noValidate
-                id="kt_login_signup_form"
               >
                 <div className="row mb-4">
                   <div className="col col-12 col-sm-8 col-md-8 col-lg-8">
@@ -61,7 +90,7 @@ export function RegisterItem() {
                     />
                   </div>
                   <div className="col col-4">
-                    <h3 className="text-dark fs-4 text-end">شے کا نام</h3>
+                    <h3 className="text-dark fs-4 text-end">آئٹم کا نام</h3>
                   </div>
                 </div>
                 {/* Plot Number */}
@@ -70,18 +99,16 @@ export function RegisterItem() {
                     <input
                       type="number"
                       value={itemQuantity}
-                      step="1"
-                      min="100"
+                      step="100"
+                      min="0"
                       onChange={(e) => {
-                        setItemQuantity(parseInt(e.target.value));
+                        setItemQuantity(parseFloat(e.target.value));
                       }}
                       className="form-control form-control-lg rounded-3 text-center"
                     />
                   </div>
                   <div className="col col-4">
-                  <h3 className="text-dark fs-4 text-end">
-                      آئٹم کی مقدار 
-                      </h3>
+                    <h3 className="text-dark fs-4 text-end">آئٹم کی مقدار</h3>
                   </div>
                 </div>
                 {/* cost */}
@@ -102,6 +129,24 @@ export function RegisterItem() {
                     <h3 className="text-dark fs-4 text-end">لاگت فی آئٹم</h3>
                   </div>
                 </div>
+                {/* Type */}
+                <div className="row mb-4">
+                  <div className="col col-12 col-sm-8 col-md-8 col-lg-8">
+                    <input
+                      type="text"
+                      value={itemType}
+                      step="100"
+                      min="0"
+                      onChange={(e) => {
+                        setItemType(e.target.value);
+                      }}
+                      className="form-control form-control-lg rounded-3 text-center"
+                    />
+                  </div>
+                  <div className="col col-4">
+                    <h3 className="text-dark fs-4 text-end">قسم(عدد/فٹ)</h3>
+                  </div>
+                </div>
                 {/* Total Price */}
                 <div className="row mb-4">
                   <div className="col col-12 col-sm-8 col-md-8 col-lg-8">
@@ -117,11 +162,14 @@ export function RegisterItem() {
                   </div>
                 </div>
                 <div className="d-flex justify-content-start">
-                  <button className="btn bg-primary text-white ActiveEffect urdu p-2 px-4">
+                  <button
+                    className="btn bg-primary text-white ActiveEffect urdu p-2 px-4"
+                    onClick={createItem}
+                  >
                     جمع کرائیں
                   </button>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
