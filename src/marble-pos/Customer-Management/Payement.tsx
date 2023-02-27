ca1fe487460dc22a7152adcbc456025feecef2a7
DESKTOP-TVR0LRO\dell <aqib.ali018634@gmail.com>
2023-02-27 05:19:35 +0500

    payement rcv set

--- src/marble-pos/Customer-Management/Payement.tsx
+++ src/marble-pos/Customer-Management/Payement.tsx
@@ -1,12 +1,12 @@
 import { Button, TextField } from "@mui/material";
-import '../../app.css'
+import "../../app.css";
 
 const PayementRCV = () => {
   return (
     <div>
       <div className="mt-2 pt-0 urdu">
         {/* Logo */}
-        <div className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center my-4">
           <div className="text-center">
             <div className="">
               <div className="d-flex justify-content-center">
@@ -15,7 +15,7 @@ const PayementRCV = () => {
             </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-2">
           <div
             className="col-12"
             style={{
@@ -30,53 +30,50 @@ const PayementRCV = () => {
               id="kt_login_signup_form"
             >
               {/* Name */}
-              <div className="row d-flex justify-content-between">
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6">
-                  <TextField
-                    size="small"
-                    id="outlined-basic"
-                    label="رقم موصول"
+              <div className="row mb-3">
+                <div className="col col-12 col-sm-8 col-md-8 col-lg-8">
+                  <input
                     type="number"
-                    className="urdu"
-                    variant="outlined"
-                    //   value={nameValue}
-                    //   onChange={(e) => {
-                    //     setNameValue(e.target.value)
-                    //   }}
+                    step="1"
+                    min="100"
+                    className="form-control form-control-lg rounded-3 text-center"
                   />
                 </div>
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6">
-                  <TextField
-                    size="small"
-                    id="outlined-basic"
-                    label="رعایت۔"
-                    type="number"
-                    className="urdu"
-                    variant="outlined"
-                    //   value={nameValue}
-                    //   onChange={(e) => {
-                    //     setNameValue(e.target.value)
-                    //   }}
-                  />
-                </div>
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 urdu">
-                  <TextField
-                    size="small"
-                    id="outlined-basic"
-                    className="urdu"
-                    label="ایڈمن پاس ورڈ"
-                    variant="outlined"
-                    type="password"
-                    //   value={nameValue}
-                    //   onChange={(e) => {
-                    //     setNameValue(e.target.value)
-                    //   }}
-                  />
+                <div className="col col-4">
+                  <h3 className="text-dark fs-4 text-end">رقم موصول</h3>
                 </div>
               </div>
+              <div className="row mb-3">
+                <div className="col col-12 col-sm-8 col-md-8 col-lg-8">
+                  <input
+                    type="number"
+                    step="1"
+                    min="100"
+                    className="form-control form-control-lg rounded-3 text-center"
+                  />
+                </div>
+                <div className="col col-4">
+                  <h3 className="text-dark fs-4 text-end">رعایت۔</h3>
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col col-12 col-sm-8 col-md-8 col-lg-8">
+                  <input
+                    type="password"
+                    step="1"
+                    min="100"
+                    className="form-control form-control-lg rounded-3 text-center"
+                  />
+                </div>
+                <div className="col col-4">
+                  <h3 className="text-dark fs-4 text-end">ایڈمن پاس ورڈ</h3>
+                </div>
+              </div>
+              
+              
               <div className="d-flex justify-content-between">
                 <Button variant="contained" className="text-white ActiveEffect my-3 urdu">
-                وصول کریں۔
+                  وصول کریں۔
                 </Button>
               </div>
             </form>
